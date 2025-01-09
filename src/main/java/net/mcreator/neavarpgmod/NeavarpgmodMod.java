65eddb243963eb8e279a1821f588de275b8c2867
CeaslessVoid <crocamedelain@gmail.com>
2025-01-10 08:57:50 +1100

    123123

--- src/main/java/net/mcreator/neavarpgmod/NeavarpgmodMod.java
+++ src/main/java/net/mcreator/neavarpgmod/NeavarpgmodMod.java
@@ -17,6 +17,8 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.FriendlyByteBuf;
 
+import net.mcreator.neavarpgmod.init.NeavarpgmodModAttributes;
+
 import java.util.function.Supplier;
 import java.util.function.Function;
 import java.util.function.BiConsumer;
@@ -37,6 +39,7 @@ public class NeavarpgmodMod {
 		MinecraftForge.EVENT_BUS.register(this);
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
 
+		NeavarpgmodModAttributes.REGISTRY.register(bus);
 		// Start of user code block mod init
 		// End of user code block mod init
 	}
